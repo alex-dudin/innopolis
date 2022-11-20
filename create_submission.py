87d06081e127b86969d3ecff6e0b5127be767fe0
Alexander Dudin <alexander.dudin@outlook.com>
2022-11-20 14:57:23 +0300

    Initial commit

---
+++ create_submission.py
@@ -0,0 +1,107 @@
+﻿#!/usr/bin/env python3
+# Python 3.9 or higher required to run this program.
+
+# pylint: disable=missing-docstring
+
+import argparse
+import json
+import logging
+import re
+import time
+from pathlib import Path
+from typing import List, NamedTuple, Optional
+
+import pandas as pd
+import numpy as np
+
+
+logger = logging.getLogger('innopolis')
+
+
+class ProgramOptions(NamedTuple):
+    input_path: List[Path]
+    output_path: Path
+    weights: Optional[List[float]] = None
+
+
+TARGET_COUNT = 7
+
+
+def create_submission(options: ProgramOptions):
+    logger.info('Read predicts...')
+    data = [pd.read_csv(path) for path in options.input_path]
+
+    logger.info('Blend predicts...')
+    predicts = []
+    for target_index in range(TARGET_COUNT):
+        sum_predict = None
+        for df_num, df in enumerate(data):
+            predict = df[f'p{target_index}']
+            if options.weights:
+                predict *= options.weights[df_num]
+
+            if sum_predict is None:
+                sum_predict = predict
+            else:
+                sum_predict += predict
+
+        predicts.append(sum_predict)
+
+    submission = data[0][['id']].copy()
+    submission['crop'] = np.argmax(predicts, axis=0)
+    
+    logger.info('Create submission...')
+    submission.to_csv(options.output_path, index=False, encoding='ascii', line_terminator='\n')
+
+
+def configure_logging():
+    formatter = logging.Formatter('%(asctime)s [%(levelname)5s] %(message)s')
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.DEBUG)
+    logger.addHandler(stream_handler)
+
+    logger.setLevel(1) # min level
+
+
+def parse_command_line_options() -> ProgramOptions:
+    parser = argparse.ArgumentParser(
+        description='Create submission for the Innopolis contest (https://lk.hacks-ai.ru/758467/champ).',
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('-i,--input-path', dest='input_path', action='append', type=Path,
+                        help='path to the input csv-file that contains predicts')
+
+    parser.add_argument('-o,--output-path', dest='output_path', type=Path, required=True,
+                        help='path to the output csv-file')
+
+    parser.add_argument('-w,--weights', dest='weights', type=str,
+                        help='comma-separated list of weights for all targets')
+
+    args = parser.parse_args()
+
+    for path in args.input_path:
+        if not path.exists():
+            parser.error(f'argument --input-path: path "{path}" not found')
+
+    if args.weights:
+        args.weights = list(map(float, args.weights.split(',')))
+        if len(args.weights) != len(args.input_path):
+            parser.error(f'argument --weights: invalid weights count {args.weights}')
+
+    return ProgramOptions(**vars(args))
+
+
+def main():
+    options = parse_command_line_options()
+
+    configure_logging()
+
+    start_time = time.perf_counter()
+    create_submission(options)
+    logger.info(f'Total time: {time.perf_counter() - start_time} sec')
+
+
+if __name__ == '__main__':
+    main()
---
+++ elevation.py
@@ -0,0 +1,116 @@
+﻿#!/usr/bin/env python3
+# Python 3.9 or higher required to run this program.
+
+# pylint: disable=missing-docstring
+
+import argparse
+import json
+import time
+from pathlib import Path
+from typing import List, NamedTuple, Optional
+
+import pandas as pd
+import pygmt
+import numpy as np
+from tqdm.auto import tqdm
+
+
+class ProgramOptions(NamedTuple):
+    input_path: Path
+    output_path: Path
+
+
+def get_polygons(geo):
+    if geo['type'] == 'Polygon':
+        return [geo['coordinates']]
+    if geo['type'] == 'MultiPolygon':
+        return geo['coordinates']
+    if geo['type'] == 'GeometryCollection':
+        return [x['coordinates'] for x in geo['geometries'] if x['type'] == 'Polygon']
+    raise ValueError('Invalid geometry type: ' + geo['type'])
+
+
+def create_elevation_data(options: ProgramOptions):
+    data = pd.read_csv(options.input_path)
+
+    if options.output_path.exists():
+        output_data = pd.read_csv(options.output_path)
+        current_values = list(output_data['elevation'])
+    else:
+        current_values = None
+
+    geo_data = list(data['.geo'].apply(json.loads))
+    geo_polygons = list(map(get_polygons, geo_data))
+
+    def get_coordinates(polygons):
+        lon = []
+        lat = []
+        for polygon in polygons:
+            for part in polygon:
+                lon.extend(x[0] for x in part)
+                lat.extend(x[1] for x in part)
+        return lat, lon
+
+    def get_latitude(polygons) -> float:
+        lat, lon = get_coordinates(polygons)
+        return (np.max(lat) + np.min(lat)) / 2
+
+    def get_longitude(polygons) -> float:
+        lat, lon = get_coordinates(polygons)
+        return (np.max(lon) + np.min(lon)) / 2
+
+    relief_cache = {}
+    #relief = pygmt.datasets.load_earth_relief('01s', region=[30, 70, 48, 62], registration='gridline')
+
+    def get_elevation(i, polygons) -> float:
+        if current_values and not np.isnan(current_values[i]):
+            return current_values[i]
+
+        lat = get_latitude(polygons)
+        lon = get_longitude(polygons)
+        lat_i = int(lat)
+        lon_i = int(lon)
+        relief = relief_cache.get((lon_i, lat_i), None)
+        if relief is None:
+            if len(relief_cache) == 25:
+                return None
+            relief = pygmt.datasets.load_earth_relief('01s', region=[lon_i, lon_i + 1, lat_i, lat_i + 1], registration='gridline')
+            relief_cache[(lon_i, lat_i)] = relief
+        return relief.sel(lon=lon, lat=lat, method='nearest').item()
+
+    elevation = list(map(get_elevation, range(len(geo_polygons)), tqdm(geo_polygons)))
+
+    results = data[['id']].copy()
+    results['elevation'] = elevation
+    results.to_csv(options.output_path, index=False, encoding='ascii', line_terminator='\n')
+
+
+def parse_command_line_options() -> ProgramOptions:
+    parser = argparse.ArgumentParser(
+        description='Create elevation data for the Innopolis contest (https://lk.hacks-ai.ru/758467/champ).',
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('-i,--input-path', dest='input_path', type=Path, required=True,
+                        help='path to the input csv-file')
+
+    parser.add_argument('-o,--output-path', dest='output_path', type=Path, required=True,
+                        help='path to the output csv-file')
+
+    args = parser.parse_args()
+
+    if not args.input_path.exists():
+        parser.error(f'argument --input-path: path "{args.input_path}" not found')
+
+    return ProgramOptions(**vars(args))
+
+
+def main():
+    options = parse_command_line_options()
+
+    start_time = time.perf_counter()
+    create_elevation_data(options)
+    print(f'Total time: {time.perf_counter() - start_time} sec')
+
+
+if __name__ == '__main__':
+    main()
